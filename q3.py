b8528926786a2a13b76a9ac1a5163baac7412f3a
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-03-20 11:36:55 -0700

    finished 1 and 2

--- q3.py
+++ q3.py
@@ -1,11 +1,10 @@
 # Read "romeo_and_juliet.txt" (The full text of Shakespeare's Romeo and Juliet)
 
-####
-#### YOUR CODE HERE 
-####
+with open("romeo_and_juliet.txt", "r") as f:
+    text = f.read()
+    for line in f:
+        if "Juliet" in line:
+            total += 1
+print(total)
 
 # Count how many times the word "Juliet" appears
-
-####
-#### YOUR CODE HERE 
-####
