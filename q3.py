47a6261599a32fb67c7ac7967264c6d7a5e98684
Venusstars1 <nkohavi2028@kehillahstudent.org>
2025-03-24 08:55:28 -0700

    completed

--- q3.py
+++ q3.py
@@ -2,9 +2,14 @@
 
 with open("romeo_and_juliet.txt", "r") as f:
     text = f.read()
+
+with open("romeo_and_juliet.txt", "r") as f:
+    total = 0
     for line in f:
         if "Juliet" in line:
             total += 1
+        else:
+            pass
 print(total)
 
 # Count how many times the word "Juliet" appears
